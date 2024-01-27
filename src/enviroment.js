5ea0eeb0bddcaf90de0598a52d3c030392508b27
Lorenzo <57161796+lpolverino@users.noreply.github.com>
2024-01-27 12:59:35 -0300

    Update enviroment.js

--- src/enviroment.js
+++ src/enviroment.js
@@ -2,7 +2,7 @@
 const getBackEnd = () =>{
     //const backend = import.meta.env.VITE_BACKEND_URL ||"https://dandy-rhinestone-trader.glitch.me/api/" ;
 
-    const backend = "https://dandy-rhinestone-trader.glitch.me/api/";
+    const backend = "https://easy-mature-pharaoh.glitch.me/api/";
 
     return backend
 }
